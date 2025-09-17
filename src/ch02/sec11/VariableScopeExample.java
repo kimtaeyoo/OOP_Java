35ca990788cdfbecd3d25ec7789dc86d9fc87830
kimtaeyoo <112602383+kimtaeyoo@users.noreply.github.com>
2025-09-17 13:42:12 +0900

    3주차 실습코드
    3주차 객프 수업 내용

---
+++ src/ch02/sec11/VariableScopeExample.java
@@ -0,0 +1,16 @@
+package ch02.sec11;
+
+public class VariableScopeExample {
+
+	public static void main(String[] args) {
+		int v1 = 15;
+		int v2 = 0;
+		
+		if(v1>10) {
+			v2 = v1 - 10;
+		}
+		int v3 = v1 + v2 + 5; //v2 변수를 사용할수 없기 때문에 컴파일 에러가 생김(수정필요)
+		
+	}
+
+}
